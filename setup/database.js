50cdeefcf11b9669974ad050e24ff9ec0f05813f
Warner Hoefakker <warner.hoefakker@live.nl>
2021-06-19 22:35:23 +0200

    -

--- setup/database.js
+++ setup/database.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ElshofFey01!',
+    host: 'app-e41a0fdc-28d3-4f19-a4a1-9f99f415feed-do-user-8319964-0.b.db.ondigitalocean.com',
+    port: '25060',
+    user: 'sitepoint',
+    password: 'yekvmbn5019nsras',
     database: 'sitepoint'
 });
 connection.connect((err) => {
@@ -12,9 +13,10 @@ connection.connect((err) => {
 });
 
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "ElshofFey01!",
+    HOST: "app-e41a0fdc-28d3-4f19-a4a1-9f99f415feed-do-user-8319964-0.b.db.ondigitalocean.com",
+    PORT: "25060"
+    USER: "sitepoint",
+    PASSWORD: "yekvmbn5019nsras",
     DB: "sitepoint",
     dialect: "mysql",
     pool: {
@@ -24,4 +26,3 @@ module.exports = {
       idle: 10000
     }
   };
-
