209770681012686a4f79e98d7b27adfa8d058e24
Warner Hoefakker <warner.hoefakker@live.nl>
2021-06-20 17:02:19 +0200

    --

--- setup/database.js
+++ setup/database.js
@@ -1,30 +1,29 @@
-// const mysql = require('mysql');
-const postgres = require('pg');
+const mysql = require('mysql');
 
-const connection = postgres.createConnection({
-    host: 'app-e41a0fdc-28d3-4f19-a4a1-9f99f415feed-do-user-8319964-0.b.db.ondigitalocean.com',
+const connection = mysql.createConnection({
+    host: 'db-mysql-ams3-58397-do-user-8319964-0.b.db.ondigitalocean.com',
     port: '25060',
-    user: 'sitepoint',
-    password: 'yekvmbn5019nsras',
-    database: 'sitepoint'
+    user: 'doadmin',
+    password: 'adewxg1k5ei5hrl7',
+    database: 'defaultdb'
 });
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
 });
 
-// module.exports = {
-//     HOST: "app-e41a0fdc-28d3-4f19-a4a1-9f99f415feed-do-user-8319964-0.b.db.ondigitalocean.com",
-//     PORT: "25060",
-//     USER: "sitepoint",
-//     PASSWORD: "yekvmbn5019nsras",
-//     DB: "sitepoint",
-//     dialect: "postgres",
-//     sslmode: "require",
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     }
-//   };
+module.exports = {
+    HOST: "db-mysql-ams3-58397-do-user-8319964-0.b.db.ondigitalocean.com",
+    PORT: "25060",
+    USER: "doadmin",
+    PASSWORD: "adewxg1k5ei5hrl7",
+    DB: "defaultdb",
+    dialect: "mysql",
+    sslmode: "require",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
