3dc0409fe593655f243c6d8562e0fdc964b5605e
Warner Hoefakker <warner.hoefakker@live.nl>
2021-06-20 15:36:58 +0200

    --

--- setup/database.js
+++ setup/database.js
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-// const mysql = require('mysql');
-//
-// const connection = mysql.createConnection({
-//     host: 'app-e41a0fdc-28d3-4f19-a4a1-9f99f415feed-do-user-8319964-0.b.db.ondigitalocean.com',
-//     port: '25060',
-//     user: 'sitepoint',
-//     password: 'yekvmbn5019nsras',
-//     database: 'sitepoint'
-// });
-// connection.connect((err) => {
-//     if (err) throw err;
-//     console.log('Connected!');
-// });
-=======
 const mysql = require('mysql');
 
 const connection = postgres.createConnection({
@@ -26,7 +11,6 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('Connected!');
 });
->>>>>>> Stashed changes
 
 module.exports = {
     HOST: "app-e41a0fdc-28d3-4f19-a4a1-9f99f415feed-do-user-8319964-0.b.db.ondigitalocean.com",
