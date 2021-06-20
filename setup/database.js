6533cb6f2d2515a240b88f752c037f9a6b902b76
Warner Hoefakker <warner.hoefakker@live.nl>
2021-06-20 17:45:28 +0200

    --

--- setup/database.js
+++ setup/database.js
@@ -1,16 +1,16 @@
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-    host: 'db-mysql-ams3-58397-do-user-8319964-0.b.db.ondigitalocean.com',
-    port: '25060',
-    user: 'doadmin',
-    password: 'o5rurt8iv516uekc',
-    database: 'defaultdb'
-});
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
-});
+// const mysql = require('mysql');
+//
+// const connection = mysql.createConnection({
+//     host: 'db-mysql-ams3-58397-do-user-8319964-0.b.db.ondigitalocean.com',
+//     port: '25060',
+//     user: 'doadmin',
+//     password: 'o5rurt8iv516uekc',
+//     database: 'defaultdb'
+// });
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log('Connected!');
+// });
 
 module.exports = {
     HOST: "db-mysql-ams3-58397-do-user-8319964-0.b.db.ondigitalocean.com",
