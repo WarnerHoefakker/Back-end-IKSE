fe8c3998423a1e631a6606e5ad5150927c341b3c
WarnerHoefakker <warner.hoefakker@live.nl>
2021-06-21 11:26:51 +0200

    --

--- setup/database.js
+++ setup/database.js
@@ -14,7 +14,7 @@
 
 module.exports = {
     HOST: "db-mysql-ams3-58397-do-user-8319964-0.b.db.ondigitalocean.com",
-    PORT: "25062",
+    PORT: 25060,
     USER: "doadmin",
     PASSWORD: "o5rurt8iv516uekc",
     DB: "sitepoint",
