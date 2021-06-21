bee86415c669a45d9a529658622236392b3540de
WarnerHoefakker <warner.hoefakker@live.nl>
2021-06-21 09:30:50 +0200

    --

--- setup/database.js
+++ setup/database.js
@@ -13,7 +13,7 @@
 // });
 
 module.exports = {
-    HOST: "db-mysql-ams3-58397-do-user-8319964-0.b.db.ondigitalocean.com",
+    HOST: "private-db-mysql-ams3-58397-do-user-8319964-0.b.db.ondigitalocean.com",
     PORT: "25060",
     USER: "doadmin",
     PASSWORD: "o5rurt8iv516uekc",
