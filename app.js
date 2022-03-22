07000d1a433134e612d497419bb529c76c90f0f3
Warner Hoefakker <warner.hoefakker@live.nl>
2022-03-22 20:29:47 +0100

    --

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var corsOptions = {
     origin: "*"
   };
 
-const db = require("./models");
-const Role = db.role;
-db.sequelize.sync();
+// const db = require("./models");
+// const Role = db.role;
+// db.sequelize.sync();
 
 // db.sequelize.sync({ force: true }).then(() => {
-//     console.log("Drop and re-sync db.");
+//     consolxe.log("Drop and re-sync db.");
 //     initial();
 // });
 
@@ -39,24 +39,22 @@ const app = express();
 
 app.use(cors(corsOptions));
 
-const productRoutes = require("./routes/product.routes");
-const authRoutes = require('./routes/auth.routes');
-const userRoutes = require('./routes/user.routes');
-const orderRoutes = require('./routes/order.routes');
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+// const productRoutes = require("./routes/product.routes");
+// const authRoutes = require('./routes/auth.routes');
+// const userRoutes = require('./routes/user.routes');
+// const orderRoutes = require('./routes/order.routes');
 
-const connection = require('./setup/database');
+// const connection = require('./setup/database');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
-app.use(authRoutes);
-app.use(userRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
+// app.use(authRoutes);
+// app.use(userRoutes);
+// app.use('/api/products', productRoutes);
+// app.use('/api/orders', orderRoutes);
 
 // app.use((req, res, next) => {
 //     res.status(404).json("404 page");
